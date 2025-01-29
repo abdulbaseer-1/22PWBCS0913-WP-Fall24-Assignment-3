0a0b7ee1ff20a4a9a8f539ff087d6982b0cd07d2
abdulbaseer-1 <abdulbaseer.s130@gmail.com>
2025-01-29 14:29:55 +0500

    Update Cart.jsx
    ok

--- frontend/src/components/Cart/Cart.jsx
+++ frontend/src/components/Cart/Cart.jsx
@@ -15,7 +15,7 @@ function Cart({ className }) {
     useEffect(() => {
         const interval = setInterval(() => { // instead of counter i used setInterval
             fetchCart();
-          }, 100);
+          }, 1000);
     }, []);
 
     const fetchCart = async () => {
