3ab98aa0b206b7be53d36f689bb519e620e3ce57
abdulbaseer-1 <abdulbaseer.s130@gmail.com>
2025-01-29 14:15:14 +0500

    Update Cart.jsx
    reduced to 50ms

--- frontend/src/components/Cart/Cart.jsx
+++ frontend/src/components/Cart/Cart.jsx
@@ -15,7 +15,7 @@ function Cart({ className }) {
     useEffect(() => {
         const interval = setInterval(() => { // instead of counter i used setInterval
             fetchCart();
-          }, 1000);
+          }, 50);
         
           return () => clearInterval(interval);
     }, []);
